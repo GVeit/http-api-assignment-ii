9b0bb524892e92b0a9c5694a510ff85482229e7d
Gabriel Veit <gkv8601@rit.edu>
2019-09-29 18:15:38 -0400

    updated

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -4,21 +4,20 @@
 // We will be working with databases in the next few weeks.
 const users = {};
 
-//function to respond with a json object
-//takes request, response, status code and object to send
+// function to respond with a json object
+// takes request, response, status code and object to send
 const respondJSON = (request, response, status, object) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.write(JSON.stringify(object));
   response.end();
 };
 
-//function to respond without json body
-//takes request, response and status code
+// function to respond without json body
+// takes request, response and status code
 const respondJSONMeta = (request, response, status) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.end();
 };
-
 
 
 // function to send response
@@ -31,54 +30,66 @@ const respond = (request, response, content, status) => {
   response.end();
 };
 
-//return user object as JSON
+// return user object as JSON
 const getUsers = (request, response) => {
   const responseJSON = {
-    users
+    users,
   };
 
   respondJSON(request, response, 200, responseJSON);
 };
 
-//function to add a user from a POST body
+// function to add a user from a POST body
 const addUser = (request, response, body) => {
-  //default json message
+  // default json message
   const responseJSON = {
     message: 'Name and age are both required.',
   };
 
-  //check to make sure we have both fields
-  //We might want more validation than just checking if they exist
-  //This could easily be abused with invalid types (such as booleans, numbers, etc)
-  //If either are missing, send back an error message as a 400 badRequest
+  // check to make sure we have both fields
+  // We might want more validation than just checking if they exist
+  // This could easily be abused with invalid types (such as booleans, numbers, etc)
+  // If either are missing, send back an error message as a 400 badRequest
   if (!body.name || !body.age) {
     responseJSON.id = 'missingParams';
     return respondJSON(request, response, 400, responseJSON);
   }
 
-  //default status code to 201 created
+  // default status code to 201 created
   let responseCode = 201;
 
-  //if that user's name already exists in our object
-  //then switch to a 204 updated status
+  // if that user's name already exists in our object
+  // then switch to a 204 updated status
   if (users[body.name]) {
     responseCode = 204;
-  } else {
-    //otherwise create an object with that name
-    users[body.name] = {};
-  }
 
-  //add or update fields for this user name
+    responseJSON.message = 'Updated Successfully';
+    // return respondJSON(request, response, responseCode, responseJSON);
+
+    // create a valid XML string with name and age tags.
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message>Updated</message>`;
+    responseXML = `${responseXML} <id>Updated Successfully</id>`;
+    responseXML = `${responseXML} </response>`;
+
+    // return response passing out string and content type
+    return respond(request, response, responseXML, 204);
+  }
+  // otherwise create an object with that name
+  users[body.name] = {};
+
+
+  // add or update fields for this user name
   users[body.name].name = body.name;
   users[body.name].age = body.age;
 
-  //if response is created, then set our created message
-  //and sent response with a message
+  // if response is created, then set our created message
+  // and sent response with a message
   if (responseCode === 201) {
     responseJSON.message = 'Created Successfully';
-    //return respondJSON(request, response, responseCode, responseJSON);
-      
-          // create a valid XML string with name and age tags.
+    // return respondJSON(request, response, responseCode, responseJSON);
+
+    // create a valid XML string with name and age tags.
     let responseXML = '<response>';
     responseXML = `${responseXML} <message>This is a successful response</message>`;
     responseXML = `${responseXML} <id>success</id>`;
@@ -93,8 +104,32 @@ const addUser = (request, response, body) => {
   return respondJSONMeta(request, response, responseCode);
 };
 
-//public exports
+// function to show not found error
+const notFound = (request, response, acceptedTypes) => {
+  // error message with a description and consistent error id
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  if (acceptedTypes[0] === 'text/xml') {
+    // create a valid XML string with name and age tags.
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message>The page you are looking for was not found</message>`;
+    responseXML = `${responseXML} <id>notFound</id>`;
+    responseXML = `${responseXML} </response>`;
+
+    // return response passing out string and content type
+    return respond(request, response, responseXML, 404);
+  }
+
+  // return our json with a 404 not found error code
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+// public exports
 module.exports = {
   getUsers,
   addUser,
+  notFound,
 };
